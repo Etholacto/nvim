372e10101c2d6b747a2e688a5eaebacaa0ade58e
Etholacto <cedric@kornacki.net>
2024-06-07 16:58:16 +0200

    treesitter changes, not working load error of the parsers

--- lua/etholacto/plugins/treesitter.lua
+++ lua/etholacto/plugins/treesitter.lua
@@ -3,34 +3,74 @@ return {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
-    config = function(_, opts)
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag", -- Automatically add closing tags for HTML and JSX
+    },
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    config = function()
       require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-        "vim",
-        "lua",
-        "c",
-        "cpp",
-        "c_sharp",
-        "diff",
-        "gdscript",
-        "java",
-        "python",
-        "sql",
-        "css",
-        "html"
-      },
- sync_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-            smart_rename = { enable = true },
-            autotag = {
-                enable = true,
-                enable_rename = true,
-                enable_close = true,
-                enable_close_on_slash = true,
-                
+        ensure_installed = {
+          "bash",
+          "c",
+          "c_sharp",
+          "cpp",
+          "css",
+          "diff",
+          "html",
+          "javascript",
+          "java",
+          "json",
+          "lua",
+          "python",
+          "sql",
+          "vim",
+          "vimdoc",
+        },
+        sync_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+        smart_rename = { enable = true },
+        autotag = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["ak"] = { query = "@block.outer", desc = "around block" },
+              ["ik"] = { query = "@block.inner", desc = "inside block" },
+              ["ac"] = { query = "@class.outer", desc = "around class" },
+              ["ic"] = { query = "@class.inner", desc = "inside class" },
+              ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
+              ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
+              ["af"] = { query = "@function.outer", desc = "around function " },
+              ["if"] = { query = "@function.inner", desc = "inside function " },
+              ["al"] = { query = "@loop.outer", desc = "around loop" },
+              ["il"] = { query = "@loop.inner", desc = "inside loop" },
+              ["aa"] = { query = "@parameter.outer", desc = "around argument" },
+              ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
             },
+            selection_modes = {
+              ["@parameter.outer"] = "v", -- charwise
+              ["@function.outer"] = "V", -- linewise
+              ["@class.outer"] = "<c-v>", -- blockwise
+            },
+            include_surrounding_whitespace = true,
+          },
+          move = {
+            enable = true,
+            goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+            goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+            goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+            goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+          },
+        },
       })
+
+      local install = require('nvim-treesitter.install')
+      install.prefer_git = false
+      install.compilers = { "clang" }
     end
   },
 }
